0bf4ef13a7e63df15c3ac9dccf9a046a544c25ce
AlcesOzdst <1032221495@mitwpu.edu.in>
2026-01-08 15:17:37 +0530

    dt:080126;L2;added function to add 2 2d arrays

--- 2d_arrays.c
+++ 2d_arrays.c
@@ -2,18 +2,43 @@
 
 #define ROWS 2
 #define COLS 2
+
 int main() {
     int arr1[ROWS][COLS];
-    int i, j;
+    int arr2[ROWS][COLS];
+    int sum[ROWS][COLS];
+    int i, j, p, q;
     for(i=0; i<ROWS; i++) {
         for(j=0; j<COLS; j++) {
-            printf("Enter the elements for [%d][%d]: ", i, j);
+            printf("Enter the elements for arr1 [%d][%d]: ", i, j);
             scanf("%d", &arr1[i][j]);
         }
     }
+    for(p=0; p<ROWS; p++) {
+        for(q=0; q<COLS; q++) {
+            printf("Enter the elements for arr2[%d][%d]: ", p, q);
+            scanf("%d", &arr2[p][q]);
+        }
+    }
+    printf("arr1 elements ar:\n");
     for(i=0; i<ROWS; i++) {
         for(j=0; j<COLS; j++){
             printf("[%d]", arr1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("arr2 elements are:\n");
+    for(p=0; p<ROWS; p++) {
+        for(q=0; q<COLS; q++) {
+            printf("[%d]", arr2[p][q]);
+        }
+        printf("\n");
+    }
+    printf("Sum of two arrays is:\n");
+    for(i=0; i<ROWS; i++){
+        for(j=0; j<COLS; j++){
+            sum[i][j] = arr1[i][j] + arr2[i][j];
+            printf("[%d]", sum[i][j]);
         }
         printf("\n");
     }
